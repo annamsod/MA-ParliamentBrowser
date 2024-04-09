13f96cfa9f162f6948acf44f4088cc198e429e81
anna <anna.soderlund@ha.ax>
2024-04-09 11:42:08 +0300

    Added detail view

--- App.js
+++ App.js
@@ -7,11 +7,13 @@ import {
   ActivityIndicator,
   FlatList,
   StyleSheet,
+  Image,
   Text,
   View,
+  TouchableOpacity,
 } from "react-native";
 
-function HomeScreen() {
+function HomeScreen({ navigation }) {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
 
@@ -54,31 +56,102 @@ function HomeScreen() {
           data={data}
           keyExtractor={({ id }) => id}
           renderItem={({ item }) => (
-            <ListItem bottomDivider>
-              {item.image && item.image.url ? (
-                <Avatar
-                  rounded
-                  source={{
-                    uri: item.image.url,
-                  }}
-                />
-              ) : (
-                <Avatar
-                  rounded
-                  icon={{
-                    name: "person-outline",
-                    type: "material",
-                    size: 26,
-                  }}
-                  containerStyle={{ backgroundColor: "#c2c2c2" }}
-                />
-              )}
-              <ListItem.Content>
-                <ListItem.Title>{item.name}</ListItem.Title>
-              </ListItem.Content>
-            </ListItem>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Detaljvy", { person: item })}
+            >
+              <ListItem bottomDivider>
+                {item.image && item.image.url ? (
+                  <Avatar
+                    rounded
+                    source={{
+                      uri: item.image.url,
+                    }}
+                  />
+                ) : (
+                  <Avatar
+                    rounded
+                    icon={{
+                      name: "person-outline",
+                      type: "material",
+                      size: 26,
+                    }}
+                    containerStyle={{ backgroundColor: "#c2c2c2" }}
+                  />
+                )}
+                <ListItem.Content>
+                  <ListItem.Title>{item.name}</ListItem.Title>
+                </ListItem.Content>
+              </ListItem>
+            </TouchableOpacity>
           )}
         />
+      )}
+    </View>
+  );
+}
+
+function DetailView({ route, navigation }) {
+  const { person } = route.params;
+  const [age, setAge] = useState(null);
+
+  const calculateAge = () => {
+    const birthday = new Date(person.birthday);
+    const today = new Date();
+    let age = today.getFullYear() - birthday.getFullYear();
+    const monthDiff = today.getMonth() - birthday.getMonth();
+    if (
+      monthDiff < 0 ||
+      (monthDiff === 0 && today.getDate() < birthday.getDate())
+    ) {
+      age--;
+    }
+    return age;
+  };
+
+  useEffect(() => {
+    // Calculate the age when the component mounts
+    const personAge = calculateAge();
+    setAge(personAge);
+  }, []);
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <View
+        style={{ width: "80%", alignItems: "center", justifyContent: "center" }}
+      >
+        {person.image && person.image.url ? (
+          <Image
+            style={{ width: "100%", aspectRatio: 1 }} // Adjust the size as needed
+            source={{ uri: person.image.url }}
+          />
+        ) : (
+          <Image
+            style={{ width: "100%", aspectRatio: 1 }} // Adjust the size as needed
+            source={require("./icon.png")}
+          />
+        )}
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.label}>Namn: </Text>
+        <Text>{person.name}</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.label}>Adress: </Text>
+        <Text>{person.address}</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.label}>Kommun: </Text>
+        <Text>{person.city}</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.label}>Födelsedag: </Text>
+        <Text>{person.birthday}</Text>
+      </View>
+      {age !== null && (
+        <View style={styles.row}>
+          <Text style={styles.label}>Ålder: </Text>
+          <Text>{age}</Text>
+        </View>
       )}
     </View>
   );
@@ -89,9 +162,22 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Alla ledamöter">
         <Stack.Screen name="Alla ledamöter" component={HomeScreen} />
+        <Stack.Screen name="Detaljvy" component={DetailView} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  label: {
+    fontWeight: "bold",
+    marginRight: 5,
+  },
+});
